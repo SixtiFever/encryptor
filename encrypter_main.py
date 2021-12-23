cee151ee256f0427ae947d972a2b974b6d7fc159
Jason <jdswift94@gmail.com>
2021-12-23 19:03:00 +0000

    running, but index error needs to be fixed. See diary log

---
+++ encrypter_main.py
@@ -0,0 +1,39 @@
+""" A PROGRAM TO ENCRYPT AND DECRYPT INPUT MESSAGES """
+import random
+
+"""
+1. Write a message
+2. Run the message through the ciper function
+3. Store ciphered message in a new object
+4. Decrypt the message
+"""
+
+cipher_array = ['a','b','c','d','e','f','g','h','i','j','k',
+'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+""" function to take input message, and scramble it """
+""" 1. take input message. Listify it.
+    2. make randome number
+    3. Assign each message value a corresponding index in ciper_array
+    4. Move values forward by the key_shift
+    5. Append and de-list in encrypted message      
+"""
+def cipher(input_message: str) -> None:
+    x = random.randint(2,6)
+    encrypted_message = list()
+    listed_message = list(input_message)
+    for element in listed_message:
+        if element in cipher_array:
+            original_index = int(cipher_array.index(element))
+            shift = original_index + x
+            encrypted_message.append(cipher_array[shift])
+    print(encrypted_message)
+
+
+cipher('a')
+
+
+
+    
+
+    
