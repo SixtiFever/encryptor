5b086191e738b1dc7e91d2fcf4209e209b5a5205
Jason <jdswift94@gmail.com>
2021-12-26 14:44:18 +0000

    de-cipher function compete. To do: Enable files to be read as cipher input. Creat config.json. '

--- encrypter_main.py
+++ encrypter_main.py
@@ -1,16 +1,14 @@
 """ A PROGRAM TO ENCRYPT AND DECRYPT INPUT MESSAGES """
 import random
 
-"""
-1. Write a message
-2. Run the message through the ciper function
-3. Store ciphered message in a new object
-4. Decrypt the message
-"""
 
+""" lists to accomodate alphabetic substitution via indexing """
 cipher_array = ['a','b','c','d','e','f','g','h','i','j','k',
 'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 cipher_array2 = cipher_array*2
+
+""" key shift to create the randomly generate alphabetic substitution index """
+key_shift = random.randint(2,6)
 
 """ function to take input message, and scramble it """
 """ 1. take input message. Listify it.
@@ -20,22 +18,51 @@ cipher_array2 = cipher_array*2
     5. Append and de-list in encrypted message      
 """
 def cipher(input_message: str) -> None:
-    x = random.randint(2,6)
     encrypted_message = list()
-    listed_message = input_message
+    listed_message = input_message.lower()
     for element in listed_message:
         if element in cipher_array:
             original_index = int(cipher_array.index(element))
-            shift = original_index + x
+            shift = original_index + key_shift
             encrypted_message.append(cipher_array2[shift])
         if element == " ":
             encrypted_message.append(element)
     return str(''.join(encrypted_message))
 
 
-def decipher()
+"""
+- Take the ciphered message
+- Reverse cipher_array2
+- For every element in the ciphered message, take element, find index in local array, and move back by the key shit.
+Appending to new variable.
+- Move the ciphered message back by the key_shit
+"""
 
-print(cipher('hello there'))
+
+def decipher(ciphred_message):
+    """ function to take the ciphered message, and decrypt it """
+    listed_ciphered_msg = list(ciphred_message)  # convert ciphered message to a list
+    deciphered_message = list() # creat new list to append to
+    reversed_array = sorted(cipher_array, reverse=True) # reverse the array
+    local_array = reversed_array*2
+    for character in listed_ciphered_msg:  # Loop through ciphered message, get index for each element, apply key shift
+        if character != ' ':
+            index = local_array.index(character)
+            de_shift = index + key_shift
+            deciphered_message.append(local_array[de_shift]) # Append to new list
+        elif character == ' ':
+            deciphered_message.append(' ')
+    return str(''.join(deciphered_message))
+
+
+
+
+    
+
+a = cipher('Fuck off you fag')
+
+print(a)
+print(decipher(a))
 
 
 
